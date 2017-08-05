da249d6956b2b2cf54e8ee5ec7e9f3c66fbb3439
varnit05 <league@WTS10.attlocal.net>
2017-08-05 11:40:29 -0700

    08/05/17

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -2,6 +2,7 @@ package IntroToArrayLists;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,10 +11,13 @@ import javax.swing.JPanel;
 
 import package2.ChuckleClicker;
 
-public class GuestBook implements ActionListener{
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
+public class GuestBook implements ActionListener {
+	// Create a GUI with two buttons. One button reads "Add Name" and the other
+	// button reads "View Names".
+	// When the add name button is clicked, display an input dialog that asks
+	// the user to enter a name. Add
+	// that name to an ArrayList. When the "View Names" button is clicked,
+	// display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
 	// Guest #1: Bob Banders
 	// Guest #2: Sandy Summers
@@ -27,6 +31,13 @@ public class GuestBook implements ActionListener{
 	public static void main(String[] args) {
 		GuestBook c = new GuestBook();
 		c.makeButtons();
+
+		ArrayList<String> names = new ArrayList<String>();
+
+		names.add("Bob Banders");
+		names.add("Sandy Summers");
+		names.add("Greg Ganders");
+		names.add("Donny Doners");
 	}
 
 	public void makeButtons() {
@@ -45,9 +56,16 @@ public class GuestBook implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getSource() == b1){
+		if (e.getSource() == b1) {
 			JOptionPane.showInputDialog("Type in your name.");
-			
+
+		}
+		if (e.getSource() == b2) {
+			for(String s : names){
+				System.out.println(s);
+				JOptionPane.showMessageDialog(null, "Guest 1: " + s);
+			}
+
 		}
 	}
 }
